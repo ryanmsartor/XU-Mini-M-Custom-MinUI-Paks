8f4725247774356d3ce6cf515d5487b31b1c3248
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-09-05 22:32:33 -0500

    auto-fix libretro port scripts to launch correctly

--- Emus/magicmini/Ports.pak/launch.sh
+++ Emus/magicmini/Ports.pak/launch.sh
@@ -19,6 +19,11 @@ fi
 # remove troublesome logging syntax from port launcher
 sed -i 's|> \"\$GAMEDIR\/log.txt\"|# > \"\$GAMEDIR\/log.txt\"|g' "$1"
 
+# change libretro ports to look in the right place
+sed -i 's|\$raloc\/retroarch|\/storage\/TF2\/Tools\/magicmini\/RetroArch.pak\/retroarch|g' "$1"
+sed -i 's|\$raconf|--config \/storage\/TF2\/Tools\/magicmini\/RetroArch.pak\/.config\/retroarch\/retroarch.cfg|g' "$1"
+
+
 # sync volume and brightness controls to minui
 while :; do
 	syncsettings.elf >/dev/null 2>&1
